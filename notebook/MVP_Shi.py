2098ef8cd56d61c1910a49d8cb13ef63e99c204c
Yongliang Shi <yongliang.michael.shi@gmail.com>
2021-01-17 18:09:00 -0600

    define a function to return a BeautifulSoup object of a request for job searching at Indeed

--- notebook/MVP_Shi.py
+++ notebook/MVP_Shi.py
@@ -9,7 +9,7 @@ import urllib
 
 from tqdm.notebook import tqdm
 
-def url_indeed(job_title, location):
+def fisrt_page_url_indeed(job_title, location):
     '''
     This function returns a URL of a job search at Indeed.com 
     based on the job title and the location.
@@ -20,6 +20,43 @@ def url_indeed(job_title, location):
     dic = {'q': job_title, 'l': location, 'sort': 'date'}
     # Convert the dictionary to a query string
     relative_url = urllib.parse.urlencode(dic)
-    # Generate the full URL
+    # Generate the full URL of the first page
     url = base_url + relative_url
     return url
+
+def first_page_soup_indeed(job_title, location):
+    '''
+    This function returns a BeautifulSoup object to hold 
+    the content of a request for job searching at Indeed.com
+    '''
+    # Generate the URL of the job search based on title and location
+    url = fisrt_page_url_indeed(job_title, location)
+    # Make the HTTP request
+    response = requests.get(url)
+    # Print the status code of the request
+    print("Status code of the request: ", response.status_code)
+    # Sanity check to make sure the document type is HTML
+    print("Document type: ", response.text[:15])
+    # Make a soup to hold the response content
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Print out the title of the content
+    print("Title of the response: ", soup.title.string)
+    return soup
+
+def extract_jobcards_indeed(job_title, location):
+    '''
+    '''
+    # Generate the URL of the job search based on title and location
+    url = url_indeed(job_title, location)
+    # Make the HTTP request
+    response = requests.get(url)
+    # Print the status code
+    print("Status Code: ", response.status_code)
+    # Sanity check to make sure the document type is HTML
+    print(response.text[:100])
+    # Make a soup to hold the response content
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Print out the title
+    print("Soup Title: ", soup.title.string)
+    
+    return soup
