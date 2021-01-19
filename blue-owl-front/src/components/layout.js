01bd95de1eb71aec554140aaa851839a0aa7fbd7
Cpt_Crunch <jasong84@gmail.com>
2021-01-18 20:59:49 -0600

    Add front-end initial project files

---
+++ blue-owl-front/src/components/layout.js
@@ -0,0 +1,60 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import GlobalStyles from '../styles/GlobalStyles'
+import Header from "./header"
+
+import 'normalize.css'
+import "./layout.css"
+import styled from "styled-components"
+
+const StyledLayout = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+`
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <GlobalStyles />
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <StyledLayout>
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </StyledLayout>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
