f6033cda1f372892ecb471b645d795a11bcd68b4
Yongliang Shi <yongliang.michael.shi@gmail.com>
2021-02-25 22:58:09 -0600

    define two new functions to compute the frequency of the top skills in each observation and to plot how the frequency change over time

--- notebook/MVP_Shi.py
+++ notebook/MVP_Shi.py
@@ -333,7 +333,8 @@ def jobs_indeed(job_title, location, max_page=35):
 ########################### Preparation #################################
 def remove_duplicates(df):
     '''
-    This function removes the duplicates in the dataframe
+    This function removes the duplicates in the dataframe based on title, location, 
+    company, job_link, and job_description
     '''
     # Define the columns for identifying duplicates
     columns = ['title', 'location', 'company', 'job_link', 'job_description']
@@ -343,17 +344,14 @@ def remove_duplicates(df):
 
 def compute_post_date(df):
     '''
-    This function computes the date of the job post based on post age
+    This function computes the date of a job posting based on its post age
     and set the date as the index of the dataframe.
     '''
     # Create an empty list to hold the post date
     post_date = []
     # For loop the column post_age and convert the values to date
     for age in df.post_age:
-        if age == 'Just posted':
-            date = datetime.date.today()
-            post_date.append(date)
-        elif age == 'Today':
+        if age == 'Just posted' or == 'Today':
             date = datetime.date.today()
             post_date.append(date)
         else:
@@ -645,15 +643,11 @@ def everygram_frequency_v1(d_words, max_len=3):
     everygram = pd.Series(everygram).value_counts()
     return everygram
 
-def top_skills_ds_v1(k, library):
+def top_skills_v1(df, k, library, library_type):
     '''
     This function accepts a positive integer k and a skillset library and 
     returns a dataframe containing the top k skills needed for data scientist positions.
     '''
-    # Import the file path
-    database = env_Shi.database
-    # Load the prepared dataframe with job search results
-    df = pd.read_json(f"{database}df_ds_tx_prepared_backup.json")
     # Create a string of all words that appear in the job description
     dic = words_variables_v1(df)
     # Compute the words frequency
@@ -662,11 +656,15 @@ def top_skills_ds_v1(k, library):
     df_skills = pd.DataFrame()
     # For loop through the library to find out the frequency of the skills mentioned in the job description
     for skill in library:
-        mask = ( everygram_frequency.index == skill)
+        mask = (everygram_frequency.index == skill)
         df =  everygram_frequency[mask]
         df_skills = pd.concat([df_skills, df])
     df_skills.columns = dic.keys()
     df_skills.sort_values(by='frequency', ascending=False, inplace=True)
+    # Reset the index
+    df_skills.reset_index(inplace=True)
+    # Rename the column name
+    df_skills.rename(columns={'index': f'top{k}_{library_type}_skills'}, inplace=True)
     return df_skills.head(k)
 
 def top_skills_ds_v2(company, k):
@@ -696,3 +694,48 @@ def top_skills_ds_v2(company, k):
         df_skills = pd.concat([df_skills, df])
     df_skills.sort_values(by=company, ascending=False, inplace=True)
     return df_skills.head(k)
+
+def add_skill_frequency(df, df_top):
+    '''
+    This function accepts the dataframe of prepared job postings and the dataframe of the top k skills
+    and adds the frequencies of the skills in each observation.
+    '''
+    # Reset the index of the df
+    df_copy = df.reset_index()
+    # Create a list of the top skills
+    skill_list = df_top.iloc[:, 0].to_list()
+    # Create an empty dictionary to hold the frequency of the skill in each observation
+    dic_frequency = {}
+    # Loop through the list of skills to compute its frequency in each observation
+    for skill in skill_list:
+        list_frequency = []
+        for string in df_copy.clean.values:
+            matches = re.findall(f" {skill} ", string)
+            frequency = len(matches)
+            list_frequency.append(frequency)
+        dic_frequency[skill]=list_frequency    
+    # Convert the dictionary into the dataframe
+    df_frequency = pd.DataFrame(dic_frequency)
+    # Add the frequencies of the top skills to the original dataframe
+    df_copy = pd.concat([df_copy, df_frequency], axis=1)
+    # Reset the date as the index
+    df_copy = df_copy.set_index('date')
+    return df_copy
+
+def plot_top_skill_ts(df, df_top):
+    '''
+    This function accetps the dataframe of preapred job postings with the frequencies of the skills
+    and plot how popular each skill changes over time. 
+    '''
+    # Set up the size of the plot
+    plt.figure(figsize=(11, 8))
+    # Create a list of the top skills
+    skill_list = df_ds_top_tech.iloc[:, 0].to_list()
+    # Resample the dataset by week and plot the mean of the frequency of each skill per job posting
+    for skill in skill_list:
+        df.resample('W')[skill].mean().plot(label=f'{skill} Weekly')
+    
+    # Name the plot
+    plt.title("How Popular the Top 5 Skills Are Over Time", fontweight='bold')
+    # Position the legend
+    plt.legend(bbox_to_anchor=(1, 1))
