fa28a7647bb969209b7ab37ca4a7b3230c826288
Yongliang Shi <yongliang.michael.shi@gmail.com>
2021-03-07 21:40:32 -0600

    add library py files

---
+++ library.py
@@ -0,0 +1,52 @@
+library_ds_tech = ['python','sql','pandas','numpy','matplotlib','scikit learn','scikitlearn','spark','hadoop',
+                'aws','amazon web services','azure','microsoft word', 'microsoft excel','excel','tableau',
+                'tensor flow','pytorch','hive','impala','matlab','etl','statistics','exploration',
+                'extraction','data wrangling','math','machine learning','data visualization','java','js',
+                'javascript','scala','r','c','c++','power bi','dashboard','linear algebra','calculus',
+                'neural networks','eda','big data','frameworks','database management','testing hypotheses',
+                'probability','data mining','perl','nosql','saas','git','github','natural language processing',
+                'nlp', 'deep learning','agile','kanban','project management','julia','devops','google cloud',
+                'pytorch','computer vision', 'deep neural networks','neural networks','amazon web services',
+                'natural language processing','extract,transform,load','mysql','structured query language']
+
+library_ds_soft = ['critical thinking','communication','problem solving','teamwork','ethics','business acumen',
+                'interpersonal skills','curiosity','storytelling','adaptability','team player','collaboration',
+                'time management','leadership','domain knowledge','creativity','decision making',
+                'verbal communication','written communication','teamwork']
+
+library_ds_general = ['python','sql','pandas','numpy','matplotlib','scikit learn','scikitlearn','spark','hadoop',
+                'aws','amazon web services','azure','microsoft word', 'microsoft excel','excel','tableau',
+                'tensor flow','pytorch','hive','impala','matlab','etl','statistics','exploration',
+                'extraction','data wrangling','math','machine learning','data visualization','java','js',
+                'javascript','scala','r','c','c++','power bi','dashboard','linear algebra','calculus',
+                'neural networks','eda','big data','frameworks','database management','testing hypotheses',
+                'probability','data mining','perl','nosql','saas','git','github','natural language processing',
+                'nlp', 'deep learning','agile','kanban','project management','julia','devops','google cloud',
+                'pytorch','computer vision', 'deep neural networks','neural networks','amazon web services',
+                'natural language processing','extract,transform,load','mysql','structured query language'
+                'critical thinking','communication','problem solving','teamwork','ethics','business acumen',
+                'interpersonal skills','curiosity','storytelling','adaptability','team player','collaboration',
+                'time management','leadership','domain knowledge','creativity','decision making',
+                'verbal communication','written communication','teamwork']
+
+library_wd_tech = ['html','css','javascript','debugging','git','github','libraries','frameworks','publishing web site',
+                'photoshop','bootstrap','jquery','search engine optimization','seo','basic graphic design','front end','back end',
+                'visual studio code','visual studio','figma','zeplin','sketch','gatspy','strapi','paint','canva','aws','azure',
+                'amazon web services','sql','mysql','nosql','node','node.js','js','json','api','google charts','d3','d3.js','react',
+                'angular','ember','vue','python','c','c++','ruby','ruby on rails','git','github','php','net','.net','java','c#',
+                'linux','go','gcp','troubleshooting','problem solving','ux','ui','ux/ui','cloud computing','netlify','net']
+
+library_wd_soft = ['critical thinking','communication','problem solving','teamwork','ethics','business acumen',
+                'interpersonal skills','curiosity','storytelling','adaptability','team player','collaboration',
+                'time management','leadership','domain knowledge','creativity','decision making',
+                'verbal communication','written communication','teamwork']
+
+library_wd_general = ['html','css','javascript','debugging','git','github','libraries','frameworks','publishing web site',
+                'photoshop','bootstrap','jquery','search engine optimization','seo','basic graphic design','front end','back end',
+                'visual studio code','visual studio','figma','zeplin','sketch','gatspy','strapi','paint','canva','aws','azure',
+                'amazon web services','sql','mysql','nosql','node','node.js','js','json','api','google charts','d3','d3.js','react',
+                'angular','ember','vue','python','c','c++','ruby','ruby on rails','git','github','php','net','.net','java','c#',
+                'linux','go','gcp','troubleshooting','problem solving','ux','ui','ux/ui','cloud computing','netlify','net''critical thinking',
+                'communication','problem solving','teamwork','ethics','business acumen','interpersonal skills','curiosity',
+                'storytelling','adaptability','team player','collaboration','time management','leadership','domain knowledge',
+                'creativity','decision making','verbal communication','written communication','teamwork']
